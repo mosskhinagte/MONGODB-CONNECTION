ead4224165f94f9eefe083bcaee68677fd07746c
mosskhinagte <moseschorei11@gmail.comm>
2021-03-07 11:18:36 -0800

    mongodeb1

---
+++ DB/Connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const URI ="mongodb+srv://moss:8253965814@cluster0.zib8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('db connected..!');
+};
+
+module.exports = connectDB;
